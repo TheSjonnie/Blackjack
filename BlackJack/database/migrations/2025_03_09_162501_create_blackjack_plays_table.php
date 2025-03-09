5844f94ae954259951a3cf0a5dc207d5d7cfa7e8
Jesse <jessedingemanse5@gmail.com>
2025-03-09 17:31:10 +0100

    Migration Created

---
+++ BlackJack/database/migrations/2025_03_09_162501_create_blackjack_plays_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blackjack_plays', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('UserId');
+            $table->unsignedBigInteger('StartCredit');
+            $table->unsignedBigInteger('FinishCredit');
+            $table->array('DealerCards');
+            $table->array('PlayerCards');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blackjack_plays');
+    }
+};
---
+++ BlackJack/database/migrations/2025_03_09_162751_create_user_profiles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('UserId');
+            $table->unsignedBigInteger('Credits');
+            $table->unsignedBigInteger('GamesPlayed');
+            $table->unsignedBigInteger('GamesWon');
+            $table->unsignedBigInteger('GamesLost');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_profiles');
+    }
+};
